da4d079dfb33ffd57026543597d7bf3f49b9a068
Bidipta-BG <gogoibidipta@gmail.com>
2022-09-18 22:13:36 +0530

    project

---
+++ src/index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const route = require('./routes/route.js');
+const { default: mongoose } = require('mongoose');
+const app = express();
+const multer = require('multer')
+
+
+app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: true }));
+app.use(multer().any())
+
+
+
+mongoose.connect("mongodb+srv://cluster0.6evf0.mongodb.net/?retryWrites=true&w=majority", {
+        dBname: 'group18Database',
+        user: 'Avijit07',
+        pass: 'Avijit@1998',
+        useNewUrlParser: true
+    })
+    .then(() => console.log("MongoDb is connected"))
+    .catch(err => console.log(err))
+
+app.use('/', route);
+
+
+app.listen(process.env.PORT || 3000, function() {
+    console.log('Express app running on port ' + (process.env.PORT || 3000))
+});
